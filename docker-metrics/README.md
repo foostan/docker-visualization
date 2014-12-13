2b3dd30fe72df4304b2ee582257d434649536651
foostan <ks@fstn.jp>
2014-12-13 22:47:35 +0900

    forword to elasticsearch

--- docker-metrics/README.md
+++ docker-metrics/README.md
@@ -1,3 +1,12 @@
 Docker metrics
 =======================
 
+Starting Container
+------------
+```
+$docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /sys/fs/cgroup/:/sys/fs/cgroup:ro \
+  --link elasticsearch:elasticsearch \
+  foostan/docker-metrics
+```
--- docker-metrics/td-agent.conf.rb
+++ docker-metrics/td-agent.conf.rb
@@ -3,18 +3,10 @@ source {
   stats_interval 5
 }
 
-match("docker.*.*") {
-  type :forward
-  send_timeout ENV['SEND_TIMEOUT']
-  recover_wait ENV['RECOVER_WAIT']
-  heartbeat_interval ENV['HEARTBEAT_INTERVAL']
-  phi_threshold ENV['PHI_THRESHOLD']
-  hard_timeout ENV['HARD_TIMEOUT']
-
-  server {
-    name :consul
-    host ENV['SERVER_HOST']
-    port ENV['SERVER_PORT']
-    weight ENV['SERVER_WEIGHT']
-  }
+match("**") {
+  type :elasticsearch
+  host ENV['ELASTICSEARCH_HOST']
+  port ENV['ELASTICSEARCH_PORT']
+  include_tag_key true
+  tag_key "_key"
 }
